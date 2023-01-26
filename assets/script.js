4555cd132821fde04d31cce4f8201ec00f864187
FrankM89 <114839168+FrankM89@users.noreply.github.com>
2023-01-26 02:10:20 -0800

    Add questions and answers array

---
+++ assets/script.js
@@ -0,0 +1,214 @@
+// Questions and answers array
+const questions = [
+    {
+        question: "Inside which element do you put JavaScript?",
+        answers: [
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "<script>",
+                correct: true,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+        ],
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        answers: [
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "var colors = ['red', 'green', 'blue']",
+                correct: true,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+        ],
+    },
+    {
+        question: "How does a FOR loop start?",
+        answers: [
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "for (i = 0; i <= 5; i++)",
+                correct: true,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+        ],
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: [
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "function myFunction()",
+                correct: true,
+            },
+            {
+                text: "",
+                correct:false ,
+            },
+        ],
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: [
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "alert('Hello World');",
+                correct: true,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+        ],
+    },
+    {
+        question: "How do you declare a JavaScript variable?",
+        answers: [
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "var varName;",
+                correct: true,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+        ],
+    },
+    {
+        question: "What is the correct JavaScript syntax to change the content of the HTML element below?"<br>
+                "<p id='demo'>This is a demonstration.</p>",
+        answers: [
+            {
+                text: "#demo.innerHTML = 'Hello World!;",
+                correct: false,
+            },
+            {
+                text: "document.getElementByName('p').innerHTML = 'Hello World!';",
+                correct: false,
+            },
+            {
+                text: "document.getElementById('demo').innerHTML = 'Hello World!';",
+                correct: true,
+            },
+            {
+                text: "document.getElement('p').innerHTML = 'Hello World!';",
+                correct: false,
+            },
+        ],
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        answers: [
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "//This is a comment",
+                correct: true,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+        ],
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: [
+            {
+                text: "<head>",
+                correct: false,
+            },
+            {
+                text: "<p>",
+                correct: false,
+            },
+            {
+                text: "Both the <head> section and the <body> section are correct",
+                correct: true,
+            },
+            {
+                text: "<body>",
+                correct: false,
+            },
+        ],
+    },
+    {
+        question: "How do you find the number with the highest value of x and y?",
+        answers: [
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+            {
+                text: "Math.max(x, y)",
+                correct: true,
+            },
+            {
+                text: "",
+                correct: false,
+            },
+        ],
+    }
+];
